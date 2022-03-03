e42026277a33c15cbaf9d6c867c28434bd30f010
Kim Schmider <hello@schmider.kim>
2022-03-03 19:38:50 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <mpi.h>
+#include <cstdio>
+
+int main(int argc, char** argv) {
+	MPI_Init(&argc, &argv);
+
+	int rank, size;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	printf("I am %d of %d\n", rank, size);
+
+	MPI_Finalize();
+}
